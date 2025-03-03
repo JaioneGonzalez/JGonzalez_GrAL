cf4a4ff49e2c0ee626468c58f360a35338394d97
JaioneGonzalez <jaitawer.2001@gmail.com>
2025-03-03 13:13:53 +0100

    adg

--- backend/src/mongo.js
+++ backend/src/mongo.js
@@ -1,5 +1,5 @@
 const mongojs = require("mongojs");
 
-const db = mongojs("mongodb://localhost:27017/GrAL", ["erabiltzaile_datuak"]);
+const db = mongojs("mongodb://mongodb:27017/GrAL", ["erabiltzaile_datuak"]);
 
 module.exports = db;
